26d17e54becee2641e108309e86c12ace2d5dfad
navered <128045047+navered@users.noreply.github.com>
2023-09-27 13:37:55 +0300

    nave

--- newProject/public/pages/dist/myStore.js
+++ newProject/public/pages/dist/myStore.js
@@ -1,3 +1,11 @@
+// class Product {
+//   constructor(
+//     public imgUrl: string,
+//     public price: number,
+//     public title: string,
+//     public description: string,
+//   ) {}
+// }
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -34,90 +42,160 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var Product = /** @class */ (function () {
-    function Product(imgUrl, price, title, description) {
-        this.imgUrl = imgUrl;
-        this.price = price;
-        this.title = title;
-        this.description = description;
-    }
-    return Product;
-}());
-function handleAddProduct(event) {
+// async function handleAddProduct(event) {
+//   try {
+//     event.preventDefault();
+//     const imgUrl = event.target.imgUrl.value;
+//     const price = event.target.price.value;
+//     const title = event.target.title.value;
+//     const description = event.target.description.value;
+//     const userEmail = getUserEmailByQuery();
+//     if (!imgUrl || !price || !title || !description || !userEmail)
+//       throw new Error("Some fields are empty");
+//     const form = document.querySelector(".myForm") as HTMLFormElement;
+//     form.reset();
+//     const newProd: Product = new Product(imgUrl, price, title, description);
+//     const postInit = {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ newProd, userEmail }),
+//     };
+//     const response = await fetch("/API/products/create-product", postInit);
+//     const { ok, newProduct } = await response.json();
+//     renderMyStore();
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+// function getUserEmailByQuery() {
+//   const urlParams = new URLSearchParams(window.location.search);
+//   return urlParams.get("email");
+// }
+// async function handleDeleteProduct(event) {
+//   // /delete-product
+// }
+// async function renderMyStore() {
+//   try {
+//     const userEmail = getUserEmailByQuery();
+//     console.log(userEmail);
+//     const response = await fetch(
+//       `/API/products/get-products-by-owner-email?email=${userEmail}`,
+//     );
+//     const { usersProducts } = await response.json();
+//     const html = usersProducts
+//       .map((product) => {
+//         return `
+//       <div class="storeGallery__productDiv" id = "${product._id}">
+//           <img src=${product.imgUrl} alt="" />
+//           <form id ="${product._id}" onsubmit="handleUpdate(event, '${product._id}')" class="fid__info">
+//           <label>title:</label>
+//             <input id="title" name="title" type="text" value = "${product.title}"></input><br>
+//             <label>price:</label>
+//             <input id="price" name="price" type="number" value = "${product.price}">$</input><br>
+//             <label> description: </label>
+//             <input id="description" name="description" value="${product.description}"></input><br>
+//             <p>author:${product.email}</p><br>
+//           <div class="likeAndCart">
+//           <button type="submit" >update</button>
+//           <button type="button" onclick='handleDeleteProdByOwner(event , "${product._id}")'> <span class="material-symbols-outlined"> delete </span></button>
+//           </form>
+//           </div>
+//         </div>`;
+//       })
+//       .join(" ");
+//     const root = document.querySelector(".storeGallery") as HTMLDivElement;
+//     root.innerHTML = html;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// async function handleUpdate(event, id) {
+//   try {
+//     event.preventDefault();
+//     const newTitle = event.target.title.value;
+//     const newPrice = event.target.price.value;
+//     const newDescription = event.target.description.value;
+//     console.log(newTitle, newPrice, newDescription);
+//     if (!newTitle || !newPrice || !newDescription)
+//       throw new Error("some filed are  empty");
+//     const patchInit = {
+//       method: "PATCH",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         id: id,
+//         title: newTitle,
+//         price: newPrice,
+//         description: newDescription,
+//       }),
+//     };
+//     const response = await fetch(
+//       "/API/products/update-product-info",
+//       patchInit,
+//     );
+//     const { ok } = await response.json();
+//     if (!ok) throw new Error("someThing wrong . the process not completed");
+//     renderMyStore();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// async function handleDeleteProdByOwner(event, id) {
+//   try {
+//     event.preventDefault();
+//     const deleteInit = {
+//       method: "DELETE",
+//       headers: { "Content-Type": "application/json" },
+//     };
+//     const response = await fetch("/API/products/delete-product", deleteInit);
+//     const { ok } = await response.json();
+//     if (!ok) throw new Error("Error deleting");
+//     renderMyStore();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// function goHomePage() {
+//   window.location.href = "../index.html";
+// }
+function handleGetUser() {
     return __awaiter(this, void 0, void 0, function () {
-        var imgUrl, price, title, description, userEmail, form, newProd, postInit, response, _a, ok, newProduct, error_1;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
+        var response, data, error_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
                 case 0:
-                    _b.trys.push([0, 3, , 4]);
-                    event.preventDefault();
-                    imgUrl = event.target.imgUrl.value;
-                    price = event.target.price.value;
-                    title = event.target.title.value;
-                    description = event.target.description.value;
-                    userEmail = getUserEmailByQuery();
-                    if (!imgUrl || !price || !title || !description || !userEmail)
-                        throw new Error("Some fields are empty");
-                    form = document.querySelector(".myForm");
-                    form.reset();
-                    newProd = new Product(imgUrl, price, title, description);
-                    postInit = {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({ newProd: newProd, userEmail: userEmail })
-                    };
-                    return [4 /*yield*/, fetch("/API/products/create-product", postInit)];
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch("API/users/get-user")];
                 case 1:
-                    response = _b.sent();
+                    response = _a.sent();
                     return [4 /*yield*/, response.json()];
                 case 2:
-                    _a = _b.sent(), ok = _a.ok, newProduct = _a.newProduct;
-                    renderMyStore();
+                    data = _a.sent();
+                    console.log(data);
                     return [3 /*break*/, 4];
                 case 3:
-                    error_1 = _b.sent();
-                    console.error(error_1.message);
+                    error_1 = _a.sent();
                     return [3 /*break*/, 4];
                 case 4: return [2 /*return*/];
             }
         });
     });
 }
-function getUserEmailByQuery() {
-    var urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get("email");
-}
-function handleDeleteProduct(event) {
+function handleGetAllUsers() {
     return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            return [2 /*return*/];
-        });
-    });
-}
-function renderMyStore() {
-    return __awaiter(this, void 0, void 0, function () {
-        var userEmail, response, usersProducts, html, root, error_2;
+        var response, data, error_2;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     _a.trys.push([0, 3, , 4]);
-                    userEmail = getUserEmailByQuery();
-                    console.log(userEmail);
-                    return [4 /*yield*/, fetch("/API/products/get-products-by-owner-email?email=" + userEmail)];
+                    return [4 /*yield*/, fetch("API/users/get-users")];
                 case 1:
                     response = _a.sent();
                     return [4 /*yield*/, response.json()];
                 case 2:
-                    usersProducts = (_a.sent()).usersProducts;
-                    html = usersProducts
-                        .map(function (product) {
-                        return "\n      <div class=\"storeGallery__productDiv\" id = \"" + product._id + "\">\n          <img src=" + product.imgUrl + " alt=\"\" />\n          <form id =\"" + product._id + "\" onsubmit=\"handleUpdate(event, '" + product._id + "')\" class=\"fid__info\">\n          <label>title:</label>\n            <input id=\"title\" name=\"title\" type=\"text\" value = \"" + product.title + "\"></input><br>\n            <label>price:</label>\n            <input id=\"price\" name=\"price\" type=\"number\" value = \"" + product.price + "\">$</input><br>\n            <label> description: </label>\n            <input id=\"description\" name=\"description\" value=\"" + product.description + "\"></input><br>\n            <p>author:" + product.email + "</p><br>\n          \n          <div class=\"likeAndCart\">\n          <button type=\"submit\" >update</button>\n          <button type=\"button\" onclick='handleDeleteProdByOwner(event , \"" + product._id + "\")'> <span class=\"material-symbols-outlined\"> delete </span></button>\n          </form>\n          </div>\n        </div>";
-                    })
-                        .join(" ");
-                    root = document.querySelector(".storeGallery");
-                    root.innerHTML = html;
+                    data = _a.sent();
+                    console.log(data);
                     return [3 /*break*/, 4];
                 case 3:
                     error_2 = _a.sent();
@@ -128,80 +206,4 @@ function renderMyStore() {
         });
     });
 }
-function handleUpdate(event, id) {
-    return __awaiter(this, void 0, void 0, function () {
-        var newTitle, newPrice, newDescription, patchInit, response, ok, error_3;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    event.preventDefault();
-                    newTitle = event.target.title.value;
-                    newPrice = event.target.price.value;
-                    newDescription = event.target.description.value;
-                    console.log(newTitle, newPrice, newDescription);
-                    if (!newTitle || !newPrice || !newDescription)
-                        throw new Error("some filed are  empty");
-                    patchInit = {
-                        method: "PATCH",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            id: id,
-                            title: newTitle,
-                            price: newPrice,
-                            description: newDescription
-                        })
-                    };
-                    return [4 /*yield*/, fetch("/API/products/update-product-info", patchInit)];
-                case 1:
-                    response = _a.sent();
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    ok = (_a.sent()).ok;
-                    if (!ok)
-                        throw new Error("someThing wrong . the process not completed");
-                    renderMyStore();
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_3 = _a.sent();
-                    console.error(error_3);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
-}
-function handleDeleteProdByOwner(event, id) {
-    return __awaiter(this, void 0, void 0, function () {
-        var deleteInit, response, ok, error_4;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    event.preventDefault();
-                    deleteInit = {
-                        method: "DELETE",
-                        headers: { "Content-Type": "application/json" }
-                    };
-                    return [4 /*yield*/, fetch("/API/products/delete-product", deleteInit)];
-                case 1:
-                    response = _a.sent();
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    ok = (_a.sent()).ok;
-                    if (!ok)
-                        throw new Error("Error deleting");
-                    renderMyStore();
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_4 = _a.sent();
-                    console.error(error_4);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
-}
-function goHomePage() {
-    window.location.href = "../index.html";
-}
+handleGetAllUsers();
